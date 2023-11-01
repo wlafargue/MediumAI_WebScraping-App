2ba7ba5da72798acc4a1dbddd8ccd34dd8f6f21b
William Lafargue-Dit-Hauret <william.lafarguedithauret@gmail.com>
2023-11-01 22:21:29 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -16,28 +16,57 @@ def scrape(url):
     soup = bs(response.content, "lxml")
 
     # Titles
-    titles = [title.text for title in soup.find_all('h2', 'be if ig ih ii ij ik il im in io ip gu gv iq ir gw is it iu iv iw ix iy iz ja jb gh gg hz ib id bj')]
+    titles = [title.text 
+              for title in soup.find_all('h2', 'be if ig ih ii ij ik il' + \
+              ' im in io ip gu gv iq ir gw is it iu iv iw ix ' + \
+              'iy iz ja jb gh gg hz ib id bj')]
 
     # Texts
     texts = [text.text for text in soup.find_all('h3')]
 
     # Pictures
-    pictures = [img['src'] for img in soup.find_all('img', "bw lh")]
+    pictures = []
+    block = soup.find('div', "hd he hf l")
+    for div in block.find_all('div', {"class": "ab"}):
+        if div.find('div', 'l er ie'): 
+            if div.find('img', "bw lh"):
+                img = div.find('img', "bw lh")
+                if img['src'] not in pictures:
+                    pictures.append(img['src'])
+            else:
+                pictures.append(None)
 
     # Authors
-    authors = [author.text for author in soup.find_all('p', "be b gm z gh hy gg hz ia ib ic id bj")]
+    authors = []
+    for author in soup('div', 'ff fg hm hn ho ab'):
+        authors.append(author.find_all('a', "af ag ah ai aj ak al am an ao ap aq ar hx ab q")[0].text)
 
     # Authors' photo
     photos = [img['src'] for img in soup.find_all('img', "l hv bx hq hr ec")]
 
     # Original webpage of articles
-    hrefs = [article.a['href'].split('?')[0] for article in soup.find_all('div', 'lc ld le lf lg l')]
+    hrefs = []
+    for article in soup.find_all('div', "ab"):
+        a = article.find_all('a', "af ag ah ai aj ak al am an ao ap aq ar as at")
+        if len(a) > 0:
+            h = a[0].find('h2')
+            if h is not None:
+                hrefs.append('https://medium.com'+a[0]['href'])
 
     # Minutes for reading
-    min_read = [int(span.text.split()[0]) for article in soup.find_all('div', 'jg jh ji jj jk ab q') for span in article.findAll('span') if 'read' in span.contents[0]]
+    min_read = []
+    for article in soup.find_all('div', 'jg jh ji jj jk ab q'):
+        for span in article.findAll('span'):
+            if 'read' in span.text:
+                min_read.append(int(span.text.split()[0]))
+                
 
     # Published date
-    date = [span.text for article in soup.find_all('div', 'jg jh ji jj jk ab q') for span in article.children if ' read' not in span.text and len(span.text)> 2]
+    date = []
+    for article in soup.find_all('div', 'jg jh ji jj jk ab q'):
+        for span in article.children:
+            if ' read' not in span.text and len(span.text)> 2:
+                date.append(span.text)
 
     data = {
         'titles' : titles,
